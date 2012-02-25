41be700d522d058c7ee5353c83331044e97d5965
godayerv@gmail.com <godayerv@gmail.com@94cb791b-7d4e-9fec-b5ad-61c63b736c48>
2012-02-25 12:29:55 +0000

    Integration of namespace feature -> PHP5.3 restrictive

--- network-applications11-12/trunk/controllers/DeleteDirController.class.php
+++ network-applications11-12/trunk/controllers/DeleteDirController.class.php
@@ -9,19 +9,18 @@ class DeleteDirController extends AbstractController {
 
     protected function action() {
 
-        function rrmdir($path) {
-            return is_file($path) ?
-                    @unlink($path) :
-                    array_map('rrmdir', glob($path . '/*')) == @rmdir($path)
-            ;
-        }
-
         $dir = ROOT . '/media/static/';
         $dirs = scandir($dir);
         foreach ($dirs as $relPath) {
             $path = ROOT . '/media/static/' . $relPath;
-            rrmdir($path);
+            $this->rrmdir($path);
         }
+    }
+
+    private function rrmdir($path) {
+        return is_file($path) ?
+                @unlink($path) :
+                array_map('rrmdir', glob($path . '/*')) == @rmdir($path);
     }
 
 }
